62a649df8348f24be2d8d59429e4a059c1eda776
JunalChowdhuryGomez <junalchowdhury2001@gmail.com>
2025-04-13 14:56:24 -0500

    TRIVIA

--- app/tests/integration/test_flask_app.py
+++ app/tests/integration/test_flask_app.py
@@ -6,13 +6,15 @@ from app.app import app as flask_app
 
 from flask.testing import FlaskClient
 
+
+"""
 @pytest.fixture
 def client():
     flask_app.config['TESTING'] = True
     with flask_app.test_client() as client:
         with flask_app.app_context():
             yield client
-
+"""
 def test_index_get(client):
     response = client.get('/')
     assert response.status_code == 200
