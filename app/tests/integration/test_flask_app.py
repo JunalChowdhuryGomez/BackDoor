d664c1f629f92a17d9b3c76a82fe0dad47c663b7
JunalChowdhuryGomez <junalchowdhury2001@gmail.com>
2025-04-13 20:59:12 -0500

    trivia

--- app/tests/integration/test_flask_app.py
+++ app/tests/integration/test_flask_app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 from app import app
 
+from quiz import Quiz, Question, save_quiz, restore_quiz
+from database import get_questions_from_db
+
+
 @pytest.fixture
 def client():
 
