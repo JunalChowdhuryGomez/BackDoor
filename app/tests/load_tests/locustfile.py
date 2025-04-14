9d64dc9ea8f07a1e35f506bf542be3e7e96fe9f2
JunalChowdhuryGomez <junalchowdhury2001@gmail.com>
2025-04-14 11:27:08 -0500

    trivia

--- app/tests/load_tests/locustfile.py
+++ app/tests/load_tests/locustfile.py
@@ -4,7 +4,3 @@ class TriviaUser(HttpUser):
     @task
     def play_trivia(self):
         self.client.get("/")
-
-    @task
-    def start_game(self):
-        self.client.post("/", data={"difficulty": "1"})
