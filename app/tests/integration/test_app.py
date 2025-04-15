066f64e2b9e95387261f0f22426a066eaa9bb56e
JunalChowdhuryGomez <junalchowdhury2001@gmail.com>
2025-04-15 13:51:07 -0500

    trivia

--- app/tests/integration/test_app.py
+++ app/tests/integration/test_app.py
@@ -1,5 +1,5 @@
 import pytest
-from app import app, save_quiz
+from app import app, save_quiz, restore_quiz
 from quiz import Quiz
 from question import Question
 import unittest
@@ -22,3 +22,20 @@ class FlaskAppTestCase(unittest.TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Selecciona la dificultad", response.data)
+
+
+    def test_save_and_restore_quiz(self):
+        quiz = Quiz()
+        q1 = Question("¿Capital del Perú?", ["Lima", "Cusco", "Arequipa", "Trujillo"], 0, "Facil")
+        quiz.add_question(q1)
+        quiz.correct_answers = 1
+        quiz.incorrect_answers = 0
+        quiz.current_question_index = 1
+
+        data = save_quiz(quiz)
+        restored_quiz = restore_quiz(data)
+
+        self.assertEqual(restored_quiz.correct_answers, 1)
+        self.assertEqual(restored_quiz.questions[0].description, "¿Capital del Perú?")
+
+
