7635e97831384d4b269104a4651b13e7e6ba19e4
JunalChowdhuryGomez <junalchowdhury2001@gmail.com>
2025-04-18 11:46:46 -0500

    actividad 7

--- Actividad-7/tests/test_belly.py
+++ Actividad-7/tests/test_belly.py
@@ -1,27 +1,27 @@
 import pytest
 from src.belly import Belly
 
+
 # test para la clase Belly
 
-# test no gruñe si no ha comido nada
 
+# test no gruñe si no ha comido nada
 def test_no_gruñe_si_no_ha_comido_nada():
     belly = Belly()
     belly.esperar(2)
     assert not belly.esta_gruniendo()
 
 
+
 # test gruñe si ha comido mucho y espera
-
-
 def test_gruñe_si_come_muchos_y_espera():
     belly = Belly()
     belly.comer(15)
     belly.esperar(2)
     assert belly.esta_gruniendo()
 
-# test no gruñe si no espera
 
+# test no gruñe si no espera
 def test_no_gruñe_si_no_espera_lo_suficiente():
     belly = Belly()
     belly.comer(20)
@@ -30,14 +30,13 @@ def test_no_gruñe_si_no_espera_lo_suficiente():
 
 
 # tesst si come fracciones pepinos
-
 def test_comer_fraccionarios():
     belly = Belly()
     belly.comer(2.5)
     assert belly.pepinos_comidos == 2.5
 
-# test cantidad de pepinos es positivo
 
+# test cantidad de pepinos es positivo
 def test_no_permite_negativos():
     belly = Belly()
     with pytest.raises(ValueError):
@@ -50,9 +49,8 @@ def test_no_permite_demasiados():
     with pytest.raises(ValueError):
         belly.comer(1001)
 
+
 # test para la predicicion de gruñido
-
-
 def test_prediccion_gruñido_true():
     belly = Belly()
     assert belly.predecir_gruñido(20, 2) == True
@@ -70,8 +68,8 @@ def test_pepinos_restantes_para_gruñir():
     belly.comer(8)
     assert belly.pepinos_para_gruñir() == 3
 
-# test para la reduccion de pepinos si ya comio mucho
 
+# test para la reduccion de pepinos si ya comio mucho
 def test_pepinos_restantes_si_ya_come_mucho():
     belly = Belly()
     belly.comer(15)
