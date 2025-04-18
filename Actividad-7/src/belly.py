62465b5b619b4938a16f4a2d414fe436e3da06d1
JunalChowdhuryGomez <junalchowdhury2001@gmail.com>
2025-04-18 11:53:33 -0500

    actividad 7

--- Actividad-7/src/belly.py
+++ Actividad-7/src/belly.py
@@ -5,6 +5,7 @@ class Belly:
         self.pepinos_comidos = 0
         self.tiempo_esperado = 0
         self.clock_service = clock_service  # Puede ser mock
+    
     # metodo resetear el estomago
     def reset(self):
         self.pepinos_comidos = 0
